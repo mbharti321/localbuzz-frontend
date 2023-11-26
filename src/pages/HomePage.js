e9ae140753d71f15de7611e028854a949570b52c
mbharti321 <mbharti321@gmail.com>
2023-11-26 21:04:35 +0530

    add testimonial styled

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,18 @@ import Layout from "../templates/Layout";
 import CallToAction from "./dashboard/CallToAction/CallToAction";
 import ExploreCategories from "./dashboard/ExploreCategories";
 import FeaturedBusinesses from "./dashboard/FeaturedBusinesses";
-
+import Testimonials from "./dashboard/Testimonials/Testimonials";
 export default function HomePage() {
   return (
     <Layout>
       <CallToAction></CallToAction>
       <ExploreCategories></ExploreCategories>
       <FeaturedBusinesses></FeaturedBusinesses>
+      <Testimonials></Testimonials> 
       {/* <BusinessSearch></BusinessSearch>
         <Filters></Filters>
         <WhyChooseUs></WhyChooseUs>
-        <Testimonials></Testimonials> */}
+        */}
     </Layout>
   );
 }
